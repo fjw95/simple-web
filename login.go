45a828af21c3e5f647ab0c7b2c2fa28183d78483
fikyfjw <vq.rhyme@gmail.com>
2016-08-04 17:32:44 +0700

    menambahkan function successLogin untuk menampilkan file html baru jika berhasil login mengganti form attribute action menjadi /success-login file html baru

--- login.go
+++ login.go
@@ -11,18 +11,26 @@ import (
 type MyMux struct {
 }
 
-func (p *MyMuxxxx) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (p *MyMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path == "/" {
 		sayhelloName(w, r)
 		return
 	} else if r.URL.Path == "/login" {
 		login(w, r)
 		return
+	} else if r.URL.Path == "/success-login" {
+		successLogin(w, r)
+		return
 	}
 	http.NotFound(w, r)
 	return
 
 }
+func successLogin(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("/home/fiky/work/src/github.com/fjw95/simple-web/success-login.html")
+	t.Execute(w, nil)
+}
+
 func sayhelloName(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm() //Parse	url	parameters	passed,	then	parse	the	response	packet	for	the	POST	body	(request	body)
 	//	attention:	If	you	do	not	call	ParseForm	method,	the	following	data	can	not	be	obtained	form
@@ -34,15 +42,18 @@ func sayhelloName(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("key:", k)
 		fmt.Println("val:", strings.Join(v, ""))
 	}
-	fmt.Fprintf(w, "Hello	astaxie!") //	write	data	to	response
+	fmt.Fprintf(w, "Welcome") //	write	data	to	response
 }
 func login(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("method:", r.Method) //get	request	method
+	//fmt.Println("method:", r.Method) //get	request	method
+
 	if r.Method == "GET" {
 		t, _ := template.ParseFiles("/home/fiky/work/src/github.com/fjw95/simple-web/login.html")
 		t.Execute(w, nil)
 	} else {
 		r.ParseForm()
+		//var data = r.Form["username"]
+
 		//	logic	part	of	log	in
 		fmt.Println("username:", r.Form["username"])
 		fmt.Println("password:", r.Form["password"])
